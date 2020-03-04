2a9f64c47b8178585a68cbcee0b8c28af21c97e8
inesalfe <31959975+inesalfe@users.noreply.github.com>
2020-03-04 12:11:40 +0000

    Add files via upload

---
+++ Código/circularBuffer.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "circularbuffer.h"
+
+RingBuffer * newRingBuffer(int capacity, size_t size){
+
+	RingBuffer * rb = (RingBuffer *) malloc(sizeof(RingBuffer));
+
+	rb->buffer 		= malloc(capacity * size);
+	rb->head 		= 0;
+	rb->tail		= 0;
+	rb->size 		= size;
+	rb->capacity 	= capacity;
+	rb->length 		= 0;
+
+	return rb;
+}
+
+int isfullRingBuffer(RingBuffer * rb){
+	if(rb->length == rb->capacity)
+		return 1;
+	else
+		return 0;
+}
+
+int isemptyRingBuffer(RingBuffer * rb){
+	if(rb->length == 0)
+		return 1;
+	else
+		return 0;
+}
+
+void writeRingBuffer(RingBuffer * rb, void * item){
+	if (isfullRingBuffer(rb) == 1)
+		rb->head = (rb->head + 1) % rb->capacity;
+	else
+		rb->length ++;
+	memcpy(&rb->buffer[rb->tail], item, rb->size);
+	rb->tail = (rb->tail + 1) % rb->capacity;
+}
+
+void clearRingBuffer(RingBuffer * rb){
+	rb->length = rb->head = rb->tail = 0;
+}
+
+void freeRingBuffer(RingBuffer * rb){
+	free(rb->buffer);
+	free(rb);
+}
+
+void printRingBuffer(RingBuffer * rb, void (* print)(void*)){
+
+	int i, idx;
+	for(i=0, idx = rb->head; i < rb->length; i++, idx = (idx + 1) % rb->capacity){
+		printf("[ buf[%d] =", i);
+		print((char*)&(rb->buffer[idx]));
+		printf("]  ");
+		if(i%3 == 2) printf("\n");
+	}
+	printf("\n");
+
+}
+
+void printDouble(void * x)
+{
+    double * tmp = (double *) x;
+    printf(" %lf ", * tmp);
+}
+
+// // Testing
+// int main(){
+
+// 	int a;
+// 	RingBuffer * rb = newRingBuffer(3, sizeof(int));
+// 	a = 1;
+// 	writeRingBuffer(rb, &a);
+// 	printRingBuffer(rb, printInt);
+// 	printf("--------\n");
+// 	a = 2;
+// 	writeRingBuffer(rb, &a);	
+// 	printRingBuffer(rb, printInt);
+// 	printf("--------\n");
+// 	a = 3;
+// 	writeRingBuffer(rb, &a);	
+// 	printRingBuffer(rb, printInt);
+// 	printf("--------\n");
+// 	a = 4;
+// 	writeRingBuffer(rb, &a);	
+// 	printRingBuffer(rb, printInt);
+// 	a = 5;
+// 	writeRingBuffer(rb, &a);	
+// 	printRingBuffer(rb, printInt);
+
+// 	// printf("%lf\n", *((double *) (&rb->buffer[1])));
+// 	printf("%d\n", *((int *) (&rb->buffer[1])));
+
+// }
---
+++ Código/data.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "data.h"
+
+#define G 9.8
+
+Consts * newConsts(double m, double M, double k, double l){
+	
+	Consts * consts = (Consts *) malloc(sizeof(Consts));
+	consts->m = m;
+	consts->M = M;
+	consts->k = k;
+	consts->l = l; 
+	return consts;
+}
+
+Coords* newCoords(double x, double v, double theta, double omega){
+
+	Coords * coords = (Coords *) malloc(sizeof(Coords));
+	coords->x = x;
+	coords->v = v;
+	coords->theta = theta;
+	coords->omega = omega;
+	return coords;
+}
+
+void printCoords(Coords * coords){
+	printf("x : %lf, v : %lf, theta : %lf, omega : %lf \n", coords->x, coords->v, coords->theta, coords->omega);
+}
+
+void solver(Coords* coords, Consts* consts, double delta_t){
+
+	double Z, A, B, C, D, E;
+	double f_x, f_theta;
+
+	Z = consts->m / consts->M;
+	A = Z * G;
+	B = Z * consts->l; 
+	C = consts->k / consts->M; 
+	D = (1 + Z) * G / consts->l;
+
+	f_x 	= (	A * sin(coords->theta) * cos(coords->theta) 
+				+ B * sin(coords->theta) * pow(coords->omega, 2) 
+				- C * coords->x) 						
+				/ (1 + Z * pow(sin(coords->theta), 2));
+	f_theta = (- Z * sin(coords->theta) * cos(coords->theta) * pow(coords->omega, 2) 
+				- D * sin(coords->theta) 
+				+ C * cos(coords->theta) * coords->x) 	
+				/ (1 + Z * pow(sin(coords->theta), 2));
+
+	coords->v 	= coords->v + f_x * delta_t; 
+	coords->x 	= coords->x + coords->v * delta_t; 
+	coords->omega = coords->omega + f_theta * delta_t;
+	coords->theta = coords->theta + coords->omega * delta_t;
+
+}
+
+// // Testing
+// int main(){
+
+// 	Coords * coords; 
+// 	Consts * consts;
+// 	double t, tf = 100, delta_t = 1e-3;
+
+// 	consts 	= newConsts(0.2, 0.3, 50.0, 0.3); 
+// 	coords 	= newCoords(0.1, 0.0, M_PI / 4, 0.0);
+
+// 	for(t = 0; t <= tf; t += delta_t){
+// 		solver(coords, consts, delta_t);
+// 		printCoords(coords);
+// 	}
+
+// 	return 0;
+// }
